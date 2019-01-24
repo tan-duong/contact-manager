d85860d6882c217782954250a5ef6d6d2a136a92
tan-duong <tanduong@live.com>
2019-01-23 19:19:33 -0600

    adding bootstrap style

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 import Contact from './components/Contact';
 import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <Contact name='John Doe' email='john@gmail.com' phone='555-555-555' />
-        <Contact name='Karen Smith' email='karen@gmail.com' phone='333-555-555' />
+        <div className="container">
+          <Contact name='John Doe' email='john@gmail.com' phone='555-555-555' />
+          <Contact name='Karen Smith' email='karen@gmail.com' phone='333-555-555' />
+        </div>
+
       </div>
     );
   }
